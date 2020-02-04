0c2160862e3e36dab8a4e0ee0d5683ad433cdccb
Stephanie Opdam <scm.opdam@gmail.com>
2020-02-04 20:29:24 +0000

    Changed title migration files to prevent bugs

--- db/migrate/20200201165419_add_arts_to_user.rb
+++ db/migrate/20200201165419_add_arts_to_user.rb
@@ -1,4 +1,4 @@
-class AddArtstoUser < ActiveRecord::Migration[5.2]
+class AddArtsToUser < ActiveRecord::Migration[5.2]
   def change
     add_reference :users, :art, foreign_key: true
   end
--- db/migrate/20200201165721_add_booking_to_users.rb
+++ db/migrate/20200201165721_add_booking_to_users.rb
@@ -1,4 +1,4 @@
-class AddBookingtoUsers < ActiveRecord::Migration[5.2]
+class AddBookingToUsers < ActiveRecord::Migration[5.2]
   def change
     add_reference :users, :booking, foreign_key: true
   end
