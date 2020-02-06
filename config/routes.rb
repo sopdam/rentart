4f0614a60f3f2401676ab164adc3fcba28d5fc43
Gigi Etienne <g.etienne.rios@gmail.com>
2020-02-06 20:16:30 +0000

    fixed routes and added form in view new

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,11 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'new/create'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
-  get 'bookings/edit'
-  get 'bookings/update'
-  get 'arts/index'
-  get 'arts/show'
-  get 'arts/new'
-  get 'arts/create'
-  get 'arts/edit'
-  get 'arts/update'
-  get 'arts/destroy'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only:  [ :show, :new, :create, :edit, :update ] do
-    resources :arts, only:  [ :index, :new, :create ]
-  end
-  resources :arts, only:  [ :show, :edit, :update, :destroy ] do
+  # resources :users, only:  [ :show, :new, :create, :edit, :update ] do
+  #   resources :arts, only:  [ :index, :new, :create ]
+  # end
+  resources :arts, only:  [ :index, :new, :show, :edit, :update, :destroy ] do
     resources :bookings, only:  [:new, :create]
   end
 
