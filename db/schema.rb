641807b2d5ed7efa9ac8db9eff5481950ea06c64
Stephanie Opdam <scm.opdam@gmail.com>
2020-02-08 14:36:32 +0000

    Finished set-up Cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_111335) do
+ActiveRecord::Schema.define(version: 2020_02_08_140045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(version: 2020_02_08_111335) do
     t.float "longitude"
     t.float "latitude"
     t.string "dimensions"
-    t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
