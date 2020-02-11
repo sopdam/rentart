220a9e674439c3fb688d4d2841be1c9c69f89688
Amelia Urbanowicz <amelia.urbanowicz@gmail.com>
2020-02-11 20:04:38 +0000

    Navigation buttons have the same width

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Art.destroy_all
+
+puts 'Creating art pieces...'
+
+# arts = Art.create([
+#   {title: 'Mona Lisa', artist: 'Leonardo Da Vinci', description: 'The most famous painting in the world', price: 1000000, city: 'Le Louvre, Paris', dimensions: '40cm x 50cm'},
+#   {title: 'The Starry Night', artist: 'Van Gogh', description: 'Impressionism', price: 50000, city: 'Amsterdam', dimensions: '35cm x 35cm'},
+#   {title: 'Guernica', artist: 'Pablo Picasso', description: 'Cubist representation of the Guernica battle', price: 40000, city: '65 Alfred Road', dimensions: '35cm x 55cm'},
+#   {title: 'The Persistance', artist: 'Salvador Dali', description: 'Surrealism taking over persistance', price: 10000, city: 'Madrid, Spain', dimensions: '40cm x 50cm'}
+# ])
+
+Art.create(title: 'Mona Lisa', artist: 'Leonardo Da Vinci', description: 'The most famous painting in the world', price: 1000000, city: 'Le Louvre, Paris', dimensions: '40cm x 50cm')
+Art.create(title: 'The Starry Night', artist: 'Van Gogh', description: 'Impressionism', price: 50000, city: 'Amsterdam', dimensions: '35cm x 35cm')
+Art.create(title: 'Guernica', artist: 'Pablo Picasso', description: 'Cubist representation of the Guernica battle', price: 40000, city: '65 Alfred Road', dimensions: '35cm x 55cm')
+Art.create(title: 'The Persistance', artist: 'Salvador Dali', description: 'Surrealism taking over persistance', price: 10000, city: 'Madrid, Spain', dimensions: '40cm x 50cm')
+
+puts 'Finished!'
